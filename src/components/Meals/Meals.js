3c77bbbf26a81ee42b27b3ea523b838499906410
Oleg Lyubuskiy <oleg.lyubuskiy@jetsoftpro.com>
2021-07-05 15:44:15 +0300

    Add meals components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import AvailableMeals from './AvailableMeals';
+import MealsSummary from './MealsSummary';
+
+const Meals = (props) => {
+    return <>
+        <MealsSummary></MealsSummary>
+        <AvailableMeals></AvailableMeals>
+    </>
+}
+
+export default Meals;
