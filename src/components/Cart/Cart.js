b59a82d60d60f77bcf5b133dd6a2078cc0d37047
Oleg Lyubuskiy <oleg.lyubuskiy@jetsoftpro.com>
2021-07-08 20:20:49 +0300

    Add remove item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,11 +13,11 @@ const Cart = (props) => {
     const hasItems = !!cartCtx.items.length;
 
     const cartItemRemoveHandler = (id) => {
-
+        cartCtx.removeItem(id);
     }
 
     const cartItemAddHandler = (item) => {
-
+        cartCtx.addItem({ ...item, amount: 1 });
     }
 
     const cartItems = <ul className={classes['cart-items']}>{
